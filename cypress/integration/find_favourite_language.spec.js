49bf8e6b43380149a3868e3b0842b0011b448675
iAquino47 <ianaquino47@gmail.com>
2019-09-27 14:40:39 +0100

    initialized project, and added feature test

---
+++ cypress/integration/find_favourite_language.spec.js
@@ -0,0 +1,16 @@
+describe('Find favourite language', () => {
+    it('Displays the most used language in a user github profile', () => {
+      cy.visit('http://127.0.0.1:5500/index.html');
+  
+      cy.get('[data-testid="userNameText"]')
+        .type('ianaquino47');
+  
+      cy.get('[data-testid="sendButton"]')
+        .click();
+  
+      cy.get('[data-testid="userNameText"]')
+        .should('have.value', '');
+  
+      cy.contains('Ruby');
+    });
+  });
